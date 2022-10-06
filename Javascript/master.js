9e495e31e78d66884bb562cb1c6e839a9232b6da
Evans Dikel Gyamfi <sdik639@gmail.com>
2022-10-06 14:07:56 +0200

    changes in Javascript

---
+++ Javascript/master.js
@@ -0,0 +1,23 @@
+/*(function () {
+        var skills = ["HTML-100%","CSS-98%","JAVASCRIPT-71%","PYTHON-65%"];
+        i = 0;
+        setInterval(function(){ $('#skillNames').fadeOut(function(){
+            $(this).html(skills[(i = (i + 1) % skills.length)]).fadeIn();
+          }); }, 6000)
+      })();*/
+
+var skills = ["HTML-100%","CSS-98%","JAVASCRIPT-71%","PYTHON-65%"];
+      var skill = document.getElementById("skillNames");
+      var count = 1;
+      var i = 0;
+      var skillindex = 0;
+
+      while(count >0){
+      
+          }
+
+        }
+
+        i = 0;
+
+      }
